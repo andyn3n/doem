a10066399b47ecff5fbbc86f4bbbaa2b749e90c8
studiojeera <annakpar@student.oulu.fi>
2025-04-10 06:36:45 +0300

    Build website

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Tabs from './tabs.jsx'
+import HeaderRow from './header.jsx'
+import Laptop from './laptop.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('tabs')).render(
+    <Tabs />
+);
+
+createRoot(document.getElementById('headerRow')).render(
+    <HeaderRow />
+);
+
+createRoot(document.getElementById('footerRow')).render(
+    <HeaderRow />
+);
+
+createRoot(document.getElementById('laptop')).render(
+    <Laptop />
+);
