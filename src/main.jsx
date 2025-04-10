bcf78edcd411a4f7282aa89425dca8f184380551
studiojeera <annakpar@student.oulu.fi>
2025-04-10 06:44:05 +0300

    Added components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './doem/index.css'
-import Tabs from './tabs.jsx'
-import HeaderRow from './header.jsx'
-import Laptop from './laptop.jsx'
+import Tabs from './doem/tabs.jsx'
+import HeaderRow from './doem/header.jsx'
+import Laptop from './doem/laptop.jsx'
 
 createRoot(document.getElementById('tabs')).render(
     <Tabs />
