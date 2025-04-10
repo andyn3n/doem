52297724ad3aee48d29785ad648bf2a1a0a5114f
studiojeera <annakpar@student.oulu.fi>
2025-04-10 06:47:39 +0300

    Updated styles links

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './doem/index.css'
-import Tabs from './doem/tabs.jsx'
-import HeaderRow from './doem/header.jsx'
-import Laptop from './doem/laptop.jsx'
+import './doem/src/index.css'
+import Tabs from './doem/src/tabs.jsx'
+import HeaderRow from './doem/src/header.jsx'
+import Laptop from './doem/src/laptop.jsx'
 
 createRoot(document.getElementById('tabs')).render(
     <Tabs />
